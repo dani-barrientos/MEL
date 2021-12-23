9b094a81fb26bdc8d70093fd4aba37d7f53a17ac
dani-barrientos <danbar46@hotmail.com>
2021-12-23 11:19:17 +0100

    configuracion clang en linux

---
+++ out/build/Clang Linux/sample/CMakeFiles/sample.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dani/trabajo/proyectos/dabal/sample/sample.cpp" "/home/dani/trabajo/proyectos/dabal/out/build/Clang Linux/sample/CMakeFiles/sample.dir/sample.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DABAL_IMPORTS"
+  "DABAL_LINUX"
+  "DABAL_POSIX"
+  "DABAL_X64_GCC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../../dabal/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/dani/trabajo/proyectos/dabal/out/build/Clang Linux/dabal/CMakeFiles/dabal.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ out/build/Clang Linux/sample/CMakeFiles/sample.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sample.dir/sample.cpp.o"
+  "sample"
+  "sample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
