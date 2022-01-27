45010603f574a9587a41b0bdeddfc04e20ca9725
dani-barrientos <danbar46@hotmail.com>
2022-01-27 19:16:39 +0100

    first execution prototype. implemented for runnable and threadpool

--- dabal/source/parameter/FloatParameter.cpp
+++
@@ -1,9 +0,0 @@
-#include <parameter/FloatParameter.h>
-using parameter::FloatParameter;
-
-DABAL_CORE_OBJECT_TYPEINFO_IMPL(FloatParameter,BaseParameter);
-
-FloatParameter::FloatParameter( const string& name, float value ): BaseParameter( name ),
-	mValue( value )
-{
-}
